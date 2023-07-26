3f1d1fe32d01abaa763d28da110ca7adaac7c4dc
Ariel Tyson <ariel30190@hotmail.com>
2023-07-25 23:43:13 -0700

    Handle input values outside of accepted range

--- Main.java
+++ Main.java
@@ -7,24 +7,46 @@ public class Main {
         final byte ANNUAL_MONTHS = 12;
         final byte PERCENTAGE = 100;
 
+        int principal = 0;
+        float monthlyInterest;
+        int numberOfPayments;
+
         Scanner input = new Scanner(System.in);
 
-        System.out.println(" Principal amount : ");
-        int principal = input.nextInt();
+        while (true) {
+            System.out.print(" Principal amount (Enter a value between 1,000 & 1,000,000) : ");
+            principal = input.nextInt();
+            if (principal >= 1_000 && principal <= 1_000_000) {
+                break;
+            }
+            System.out.println(" Value is outside of accepted range. Please re-enter a value between 1,000 & 1,000,000. ");
+        }
 
-        System.out.println(" Annual Interest Rate : ");
-        float annualInterest = input.nextFloat();
-        float monthlyInterest = annualInterest / PERCENTAGE / ANNUAL_MONTHS;
+        while (true) {
+            System.out.print(" Annual Interest Rate (Enter a value between 1 & 30) : ");
+            float annualInterest = input.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENTAGE / ANNUAL_MONTHS;
+                break;
+            }
+            System.out.println(" Value is outside of accepted range. Please re-enter a value between 1 & 30. ");
+        }
 
-        System.out.println(" Period in (years) : ");
-        byte years = input.nextByte();
-        int numberOfPayments = years * ANNUAL_MONTHS;
+        while (true) {
+            System.out.print(" Time period (in years, between 1 & 30) : ");
+            byte years = input.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * ANNUAL_MONTHS;
+                break;
+            }
+            System.out.println(" Value is outside of accepted range. Please re-enter a value between 1 and 30. ");
+        }
 
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) -1);
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println( " Mortgage: " + mortgageFormatted);
-
     }
 }
